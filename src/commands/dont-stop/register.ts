5f844e670b8d3535155c92dbcb801c22614124df
Nick Tune <nick@ntcoding.co.uk>
2026-04-25 17:17:21 +0100

    fix: make dont-stop templates neutral

--- src/commands/dont-stop/register.ts
+++ src/commands/dont-stop/register.ts
@@ -4,8 +4,8 @@ import { buildCommandName } from "../../plugin-registry/command-names.js"
 export const DONT_STOP_COMMAND_NAME = buildCommandName("dont-stop")
 export const CLEAR_DONT_STOP_COMMAND_NAME = buildCommandName("clear-dont-stop")
 
-const DONT_STOP_TEMPLATE = "Enable dont-stop for this session."
-const CLEAR_DONT_STOP_TEMPLATE = "Disable dont-stop for this session."
+const DONT_STOP_TEMPLATE = "dont-stop enabled"
+const CLEAR_DONT_STOP_TEMPLATE = "dont-stop cleared"
 
 export function registerDontStopCommands(commandConfig: Record<string, CommandDefinition>): void {
   if (!commandConfig[DONT_STOP_COMMAND_NAME]) {
